aca64d61090e364c8b36508e291cff8a0d964116
신지훈 <130414837+Jihoon0127@users.noreply.github.com>
2023-11-27 15:08:23 +0900

    [level unrated] Title: 중복된 숫자 개수, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -0,0 +1,15 @@
+function solution(array, n) {
+    i = 0;
+   count = 0;
+    for(i=0 ; array.length-1>=i;i++){
+      if(array[i]==n){
+          count++;
+      }else{
+          count+0;
+      }
+        
+    
+    }
+    return count;
+    
+}
