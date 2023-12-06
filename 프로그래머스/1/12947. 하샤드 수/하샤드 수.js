0f662ad7fdb6c44f141b76fd4557fdb2b7140894
신지훈 <130414837+Jihoon0127@users.noreply.github.com>
2023-12-07 00:16:05 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,13 @@
+function solution(x) {
+   let result = String(x).split('');
+   let sum = 0;
+   for(let i = 0; i < result.length; i++) {
+       sum += Number(result[i]);
+   }
+
+   if(x % sum === 0){
+      return true;
+   } else {
+      return false;
+   }   
+}
