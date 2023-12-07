55e03958c265357e9a79e63ccbc2274e807ad83c
신지훈 <130414837+Jihoon0127@users.noreply.github.com>
2023-12-08 00:31:43 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,10 @@
+function solution(seoul) {
+    let i = 0;
+    for(i = 0; seoul.length>=i ; i++){
+        if(seoul[i]=='Kim'){
+            return `김서방은 ${i}에 있다`;
+        }
+    }
+    
+    
+}
