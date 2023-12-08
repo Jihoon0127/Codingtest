84170cfe1d576bfb240a845d1b4408a742c3fc34
신지훈 <130414837+Jihoon0127@users.noreply.github.com>
2023-12-08 21:34:58 +0900

    [level unrated] Title: 문자 반복 출력하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,16 @@
+function solution(my_string, n) {
+       
+    
+    let result= ''
+    let sum = ''
+    for (let i =0 ; my_string.length>i;i++){
+        for(let j = 0 ; j < n ; j++){
+        sum += my_string[i];
+    }
+    }
+    return sum
+   
+    
+    
+
+}
