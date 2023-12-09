1589a4179af12a90662a121b095c1bb8352ea0a4
신지훈 <130414837+Jihoon0127@users.noreply.github.com>
2023-12-09 16:28:36 +0900

    [level unrated] Title: 머쓱이보다 키 큰 사람, Time: 3.49 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,12 @@
+function solution(array, height) {
+    let result = 0;
+    let arr = array.sort((a,b)=>a-b);
+    console.log(arr)
+    
+    for(let i = 0 ; array.length-1 >=i ; i++){
+        if(array[i]>height){
+            result++;
+        }
+    }
+    return result;
+}
