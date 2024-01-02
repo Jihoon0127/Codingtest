b6604aa6bcd4a50caa245353c440071bb309bc90
신지훈 <130414837+Jihoon0127@users.noreply.github.com>
2024-01-02 15:56:03 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,6 +1,6 @@
 function solution(s) {
     
-     if(s.length %2 ==1){
+     if(s.length%2 ==1){
         return s[Math.floor(s.length/2)]
     }else if (s.length %2 ==0){
         return s[Math.floor(s.length/2-1)]+s[Math.floor(s.length/2)]    
