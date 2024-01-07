ef257bf9c684e9b884dba08afe0bcb9ba5bda9c1
신지훈 <130414837+Jihoon0127@users.noreply.github.com>
2024-01-08 03:41:56 +0900

    [level unrated] Title: 카운트 다운, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
@@ -0,0 +1,9 @@
+function solution(start, end_num) {
+    let result = []
+    let startnum = start;
+    for(let i = 0 ; i <= start-end_num ; i++){
+        result[i] = startnum--;
+       
+    }
+    return result;
+}
