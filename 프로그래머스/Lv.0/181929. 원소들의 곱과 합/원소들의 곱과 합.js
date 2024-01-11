5ebcdb9e2c86615e93e349e5b75dbf1387f8feae
신지훈 <130414837+Jihoon0127@users.noreply.github.com>
2024-01-12 03:53:06 +0900

    [level Lv.0] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/Lv.0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,13 +1,3 @@
 function solution(num_list) {
-    let result = 1;  
-    let result1 = num_list.reduce((a,b)=>a+b)*num_list.reduce((a,b)=>a+b);
-    for(let i = 0 ; i<num_list.length ; i++){
-          result *= num_list[i]
-      }
-   if(result < result1){
-       return 1
-   }else{
-       return 0;
-   }
-
+    return Math.pow(num_list.reduce((a,b) => a+b), 2) > num_list.reduce((a,b) => a*b) ? 1 : 0;
 }
