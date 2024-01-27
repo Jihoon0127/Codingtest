f9514eef19d2545663430eb43e7c412bf2173f2b
신지훈 <130414837+Jihoon0127@users.noreply.github.com>
2024-01-28 01:44:04 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 3.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let abc = my_string.split('');
+      let sum = 0;
+    let num = abc.filter((a)=> Number(a))
+    console.log(num)
+    for(let i = 0 ; num.length-1>=i ; i++){
+        sum+= parseInt(num[i])
+    }
+    
+    return sum ;
+}
