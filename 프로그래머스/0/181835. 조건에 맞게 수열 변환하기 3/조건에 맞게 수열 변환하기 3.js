03e71b3a90ffa338139e0e070429a97f0267d6a0
신지훈 <130414837+Jihoon0127@users.noreply.github.com>
2024-02-04 04:34:48 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 269.71 ms, Memory: 44 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -0,0 +1,12 @@
+function solution(arr, k) {
+    for(let i = 0 ; arr.length>i ; i++){
+         if(k%2 ==0){
+             arr[i]+=k;
+         }else{
+             arr[i]*=k;
+         }
+    }
+    return arr
+    
+   
+}
