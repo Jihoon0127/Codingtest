bd3b1b4387e123f6c6cc9660daacb3903ba20262
신지훈 <130414837+Jihoon0127@users.noreply.github.com>
2024-02-15 03:41:47 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,12 +1,3 @@
 function solution(my_string) {
-    let result =[]
-    for(let i = 0 ; i<my_string.length;i++){
-        if(my_string[i] ===my_string[i].toUpperCase()){
-            result.push(my_string[i].toLowerCase())
-        }else{
-            result.push(my_string[i].toUpperCase())
-        }
-        
-    }
-    return result.join("")
+    return my_string.split("").map((a) => a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()).join("")
 }
