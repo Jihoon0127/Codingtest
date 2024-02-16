e08a9a7bb55698e23cc4ada832867fdd87afc1ad
신지훈 <130414837+Jihoon0127@users.noreply.github.com>
2024-02-17 03:17:08 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
@@ -1,10 +1,4 @@
 function solution(num_list) {
-   let result = num_list.sort((a,b)=>a-b);
-   let result1 = [] 
-    for(let i = 0 ; i<result.length;i++){
-        if(i >=5){
-            result1[i-5] = result[i]
-        }
-    }
-    return result1
+   return num_list.sort((a,b)=>a-b).splice(5);
+  
 }
